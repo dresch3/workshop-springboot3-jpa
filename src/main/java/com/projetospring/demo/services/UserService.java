60ffcf30bdb9725e00fb9d7e7e9f110dc6fe0506
Diogo Dresch <dresch1999@gmail.com>
2025-02-16 07:28:47 -0300

    Exception handling - update

--- src/main/java/com/projetospring/demo/services/UserService.java
+++ src/main/java/com/projetospring/demo/services/UserService.java
@@ -12,6 +12,8 @@ import com.projetospring.demo.repositories.UserRepository;
 import com.projetospring.demo.services.exceptions.DatabaseException;
 import com.projetospring.demo.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 	
@@ -45,9 +47,13 @@ public class UserService {
 
 	
 	public User update(Long id, User obj) {
+		try {
 		User entity = repository.getReferenceById(id);
 		updateData(entity, obj);
 		return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
